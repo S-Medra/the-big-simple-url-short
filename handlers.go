55ce2ff03716bce6a0164d49b7bc1fdd8c4e0d79
Saeed Ahmed <saeedmedra@gmail.com>
2025-12-23 23:22:40 +0200

    refactor: move handlers into a separate file

---
+++ handlers.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"html/template"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const port = "8080"
+
+type URLShortener struct {
+	urls map[string]string
+}
+
+type ShortenPageData struct {
+	OriginalURL  string
+	ShortenedURL string
+}
+
+func (us *URLShortener) HandleForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, nil)
+}
+
+func (us *URLShortener) HandleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	originalURL := r.FormValue("url")
+	if originalURL == "" {
+		http.Error(w, "URL parameter is missing", http.StatusBadRequest)
+		return
+	}
+
+	shortKey := generateShortKey()
+	us.urls[shortKey] = originalURL
+
+	shortenedURL := "http://localhost:" + port + "/short/" + shortKey
+
+	tmpl, err := template.ParseFiles("shorten.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	data := ShortenPageData{
+		OriginalURL:  originalURL,
+		ShortenedURL: shortenedURL,
+	}
+
+	w.Header().Set("Content-Type", "text/html")
+	tmpl.Execute(w, data)
+}
+
+func (us *URLShortener) HandleRedirect(w http.ResponseWriter, r *http.Request) {
+	shortKey := strings.TrimPrefix(r.URL.Path, "/short/")
+	if shortKey == "" {
+		http.Error(w, "Shortened key is missing", http.StatusBadRequest)
+		return
+	}
+
+	originalURL, found := us.urls[shortKey]
+	if !found {
+		http.Error(w, "Shortened key not found", http.StatusNotFound)
+		return
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusFound)
+}
+
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func generateShortKey() string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const keyLength = 6
+
+	shortKey := make([]byte, keyLength)
+	for i := range shortKey {
+		shortKey[i] = charset[rng.Intn(len(charset))]
+	}
+	return string(shortKey)
+}
